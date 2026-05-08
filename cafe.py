6eafbddb8e7ecd36194fbd7e8592d0452fb1dfcb
danikua227 <152099928+danikua227@users.noreply.github.com>
2026-05-08 15:00:14 -0500

    Add files via upload

---
+++ cafe.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(page_title="RoastLab AI", layout="wide")
+
+st.title("COFFEE MAILARD TECH")
+st.markdown("### Software inteligente de tostión basado en la Reacción de Maillard")
+
+st.markdown("""
+Bienvenido al sistema:
+
+-  Simulador de tostión
+-  Negocio
+-  Inteligencia artificial
+
+Usa el menú lateral para navegar.
+""")
